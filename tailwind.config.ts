92c65ee4904462190f332a3629014b3bf20a2941
vyu <vyuvyuvyu@naver.com>
2024-07-18 16:43:58 +0900

    feat: new plugin with tailwind CSS

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,21 @@
-import type { Config } from 'tailwindcss';
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {},
-  plugins: [],
+  theme: {
+    extend: {
+      margin: {
+        tomato: "120px",
+      },
+      borderRadius: {
+        "sexy-name": "11.11px",
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/forms")],
 };
 export default config;
